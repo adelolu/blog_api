5a246a9c66fcf5488b84e3b3fa0fdbb60c1d8acc
temiloluwa oyinloye <adelolu6@gmail.com>
2024-09-02 16:22:32 +0100

    fix moved rimraf to dependencies

--- dist/routes/admin.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-// router.get("/post", verifyAccess([UserRoles.admin]), getAllPosts);
-// router.delete("/post/:postId", verifyAccess([UserRoles.admin]), deletePost);
-// export default router;
-// export default (app: Router) => app.use("/admin", router);
-//# sourceMappingURL=admin.js.map
